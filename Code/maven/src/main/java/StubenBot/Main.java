e46aceb28b2c58631fdb089205de738f3f6dfad6
TheDiffi <64838662+TheDiffi@users.noreply.github.com>
2024-11-24 15:27:50 +0100

    added .env support

--- Code/maven/src/main/java/StubenBot/Main.java
+++ Code/maven/src/main/java/StubenBot/Main.java
@@ -21,6 +21,7 @@ import discord4j.core.object.entity.Role;
 import discord4j.core.object.entity.User;
 import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.rest.util.Color;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
     public static final String prefix = "%";
@@ -29,66 +30,88 @@ public class Main {
     public static TreeMap<String, Sticker> registeredStickers = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
     public static ArrayList<AuthID> authorizations = new ArrayList<>();
     public static GatewayDiscordClient client;
+
     public static void main(String[] args) throws Exception {
+        String token = getTokenBasedOnEnv(args);
 
         bootup();
 
         // loads the Bot api
-        //Token in the .env file
-        client = DiscordClientBuilder.create("")
-            .build()
-            .login()
-            .block();
+        client = DiscordClientBuilder.create(token)
+                .build()
+                .login()
+                .block();
 
-
-               // looks at every message and calls "handleCommands"
+        // looks at every message and calls "handleCommands"
         client.getEventDispatcher().on(ReadyEvent.class)
-        .subscribe(event -> {
-            try {
-                final User self = event.getSelf();
-                System.out.println(String.format(
-                    "Logged in as %s#%s", self.getUsername(), self.getDiscriminator()
-                ));
-            } catch (Exception ex) {
-                ex.printStackTrace();
-                System.out.println("Ran into a critical error while starting!");
-            }
-        });
+                .subscribe(event -> {
+                    try {
+                        final User self = event.getSelf();
+                        System.out.println(String.format(
+                                "Logged in as %s#%s", self.getUsername(), self.getDiscriminator()));
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        System.out.println("Ran into a critical error while starting!");
+                    }
+                });
 
         client.getEventDispatcher().on(MessageCreateEvent.class)
-        .filter(message -> message.getMessage().getAuthor().map(user -> !user.isBot()).orElse(false))
-        .subscribe(event ->{
-            try {
-                CommandDistributer.handleCommands(event);
-            } catch (Exception e) {
-                Globals.createEmbed(event.getMessage().getChannel().block(), Color.RED, "Critical Error ⚠️", 
-                "avoided critical Exception, pls don't repeat what u did *laughs in pain*  😰 \n" +
-                "But don't worry a Developer has already been notifed ✉️"
-                );
+                .filter(message -> message.getMessage().getAuthor().map(user -> !user.isBot()).orElse(false))
+                .subscribe(event -> {
+                    try {
+                        CommandDistributer.handleCommands(event);
+                    } catch (Exception e) {
+                        Globals.createEmbed(event.getMessage().getChannel().block(), Color.RED, "Critical Error ⚠️",
+                                "avoided critical Exception, pls don't repeat what u did *laughs in pain*  😰 \n" +
+                                        "But don't worry a Developer has already been notifed ✉️");
 
-                //dtf does not stand for waht you think it stands for :)
-                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");  
-                LocalDateTime now = LocalDateTime.now();  
+                        // dtf does not stand for waht you think it stands for :)
+                        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+                        LocalDateTime now = LocalDateTime.now();
 
-                MessageChannel channel = (MessageChannel) client.getChannelById(Snowflake.of("866007421738025020")).block();
-                Role dev = client.getRoleById(Snowflake.of("707981224773288009"), Snowflake.of("866254682360119298")).block();
+                        MessageChannel channel = (MessageChannel) client
+                                .getChannelById(Snowflake.of("866007421738025020")).block();
+                        Role dev = client
+                                .getRoleById(Snowflake.of("707981224773288009"), Snowflake.of("866254682360119298"))
+                                .block();
 
-                //channel.createMessage(dev.getMention()).block();
-                Globals.createEmbed(channel, Color.GREEN, "Bot ran into an error --- " + dtf.format(now), 
-                    e.toString()       
-                );
+                        // channel.createMessage(dev.getMention()).block();
+                        Globals.createEmbed(channel, Color.GREEN, "Bot ran into an error --- " + dtf.format(now),
+                                e.toString());
 
-                //this doesnt seem to do anything
-                if(event.getGuildId().get().asString().equals("703300248843583569")){
-                    event.getMessage().getChannel().block().createMessage(e.toString()).block();
-                }
+                        // this doesnt seem to do anything
+                        if (event.getGuildId().get().asString().equals("703300248843583569")) {
+                            event.getMessage().getChannel().block().createMessage(e.toString()).block();
+                        }
 
-            }
-        }
-        );
+                    }
+                });
         System.out.println("READY!");
         client.onDisconnect().block();
 
+    }
+
+    private static String getTokenBasedOnEnv(String[] args) {
+        // Load environment variables from .env file
+        Dotenv dotenv = Dotenv.load();
+        String token;
+
+        // Check if the -prod flag is present
+        boolean isProd = false;
+        for (String arg : args) {
+            if (arg.equals("--prod")) {
+                isProd = true;
+                break;
+            }
+        }
+
+        // Use MAIN_TOKEN if -prod flag is present, otherwise use DEV2_TOKEN
+        if (isProd) {
+            token = dotenv.get("MAIN_TOKEN");
+        } else {
+            token = dotenv.get("DEV2_TOKEN");
+        }
+        return token;
     }
 
     public static void bootup() throws IOException, JsonException {
@@ -99,9 +122,6 @@ public class Main {
         // reads Authorizations
         authorizations = Authorizer.readAuthorizedIDs(authFilepath);
 
-        
     }
 
 }
-
- 
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # StubenBot
+
 This is a Discord bot that does amazing things like offering a way to send stickers like on whatsapp.
 I am aware that recently this feature got added by discord natively, this bot predates that time.
-Still, as far as i know, stickers are limited and Nitro exclusive, so I guess all that work wasn't *completely* useless :')
+Still, as far as i know, stickers are limited and Nitro exclusive, so I guess all that work wasn't _completely_ useless :')
+
+### How to use
+
+1. Get the jar file from the releases page
+2. Get a bot token from the Discord Developer Portal
+3. In your folder, create a .env file with the following content:
+
+```env
+MAIN_TOKEN=your_token_here
+```
+
+4. Copy the folders `stickers` and `local` to the same folder as the jar file. Clear the files in them of any content.
+5. Run the jar file with `java -jar StubenBot.jar --prod`
